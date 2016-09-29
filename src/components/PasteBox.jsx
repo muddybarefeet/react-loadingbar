1641ec97a49c6bd37bc50ae059e86ef8b988219e
Anna Rogers <2muddybarefeet@gmail.com>
2016-09-29 14:47:22 -0700

    clean up old code

---
+++ src/components/PasteBox.jsx
@@ -0,0 +1,26 @@
+'use strict';
+
+var React = require('react');
+var reactDOM = require('react-dom');
+var tsv = require('tsv');
+
+
+var PasteBox = React.createClass({
+  getInitialState: function(){
+    return {};
+  },
+  
+  render: function(){
+
+
+    return (
+
+      <div>
+        
+      </div>
+
+    );
+  }
+})
+
+module.exports = PasteBox;
